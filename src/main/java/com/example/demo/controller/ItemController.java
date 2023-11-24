d75690b58ffb895105698001104471573f3ccbaa
villageworks <villageworks@gmail.com>
2023-11-24 18:47:35 +0900

    ver.0.13: カテゴリー別商品表示用リンク表示

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -7,18 +7,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Category;
 import com.example.demo.entity.Item;
+import com.example.demo.repository.CategoryRepository;
 import com.example.demo.repository.ItemRepository;
 
 @Controller
 public class ItemController {
+	@Autowired
+	CategoryRepository categoryRepository;
 	@Autowired
 	ItemRepository itemRepository;
 	
 	// 商品一覧表示
 	@GetMapping("/items")
 	public String index(Model model) {
-		// すべての商品を取得
+		// カテゴリ検索リンク用のカテゴリーリストを取得
+		List<Category> categoryList = categoryRepository.findAll();
+		// 取得したカテゴリーリストをスコープに登録
+		model.addAttribute("categoryList", categoryList);
+		
+		// カテゴリーIDが送信されない場合：すべての商品を取得
 		List<Item> itemList = itemRepository.findAll();
 		// 取得した商品リストをスコープに登録
 		model.addAttribute("itemList", itemList);
