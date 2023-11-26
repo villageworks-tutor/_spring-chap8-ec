d54567d2d0e83a798ffd8f69b2db716093d50239
villageworks <villageworks@gmail.com>
2023-11-26 21:46:24 +0900

    ver.0.33: 顧客情報確認画面表示

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,13 +1,35 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Customer;
 
 @Controller
 public class OrderController {
+	
 	// 顧客情報入力画面表示
 	@GetMapping("/order")
 	public String index() {
 		return "customerForm";
+	}
+	
+	// 注文内容確認画面表示
+	@PostMapping("/order/confirm")
+	public String confirm(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "address", defaultValue = "") String address,
+			@RequestParam(name = "tel", defaultValue = "") String tel,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			Model model) {
+		// リクエストパラメータをもとにして顧客のインスタンスを生成
+		Customer customer = new Customer(name, address, tel, email);
+		// スコープに登録
+		model.addAttribute("customer", customer);
+		// 画面遷移
+		return "orderConfirm";
 	}
 }
